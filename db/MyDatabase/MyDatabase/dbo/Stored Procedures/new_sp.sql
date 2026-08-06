91c1591ecdecd9f55945a5cc5dee07646dd59852
Deepak0Gupta <dg82680100@gmail.com>
2026-08-06 14:31:59 +0530

    new sp added

---
+++ db/MyDatabase/MyDatabase/dbo/Stored Procedures/new_sp.sql
@@ -0,0 +1,17 @@
+﻿ 
+CREATE PROCEDURE sp_new_sp
+@ProductId int,
+ @ProductName varchar(500),
+ @AvailableQty int,
+ @ReOrderPoint int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	update Inventory
+	set ProductName = @ProductName , AvailableQty = @AvailableQty, ReOrderPoint = @ReOrderPoint
+	where ProductId=@ProductId
+   
+END
